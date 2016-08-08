8857ae7e291a6a6fdb2fe0f1adbc3d29a99a0cf5
Midhet <mdhthesenzade@gmail.com>
2016-08-08 17:21:53 +0500

    ELIAGA ISHLEYIR

--- resources/views/dizayn/personreg.blade.php
+++ resources/views/dizayn/personreg.blade.php
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	var maxField = 10; //Input fields increment limitation
 	var addButton = $('.add_button'); //Add button selector
 	var wrapper = $('.field_wrapper'); //Input field wrapper
-	var fieldHTML = '<div><div style="margin-left:10px;width:81%;height:2px;background:black;"></div><input type="text" name="field_name[]" value="" placeholder="Ad" /><input placeholder="Soyad" type="text" name="field_name[]" value=""/><input placeholder="Ata adı	" type="text" name="field_name[]" value=""/><input type="date" name="field_name[]" value=""/><input placeholder="Şəxsiyyət Vəsiqəsinin nömrəsi" type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button" title="Remove field"><img src="/dizayn/img/remove-icon.png"/></a></div>'; //New input field html
+	var fieldHTML = '<div><input id="inp"  type="text" name="field_name[]" value="" placeholder="Ad" /><input  id="inp"  placeholder="Soyad" type="text" name="field_name[]" value=""/><input id="inp"  placeholder="Ata adı	" type="text" name="field_name[]" value=""/><input id="inp"  type="date" name="field_name[]" value=""/><input id="inp"  placeholder="Şəxsiyyət Vəsiqəsinin nömrəsi" type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button" title="Remove field"><img src="/dizayn/img/remove-icon.png"/></a></div>'; //New input field html
 	var x = 1; //Initial field counter is 1
 	$(addButton).click(function(){ //Once add button is clicked
 		if(x < maxField){ //Check maximum number of input fields
@@ -34,23 +34,23 @@ input{
 }
 input[type="submit"]{ width: 40%;}
 input[type="date"]{ width: 40%;height:40px; vertical-align:top;}
-input[type="text"]{height:40px; vertical-align:top; width:40%;}
+#inp{height:40px; vertical-align:top; width:40%;}
 .field_wrapper div{ margin-bottom:10px;}
 .add_button{ margin-top:10px; margin-left:10px;vertical-align: text-bottom;}
 .remove_button{ margin-top:10px; margin-left:10px;vertical-align: text-bottom;}
 </style>
-</head>
-<body>
+
+
 <form name="codexworld_frm" action="" method="post">
 <div class="field_wrapper">
 	<div>
-    	<input placeholder="Ad" type="text" name="field_name[]" value=""/><input placeholder="Soyad" type="text" name="field_name[]" value=""/><input placeholder="Ata adı	" type="text" name="field_name[]" value=""/><input type="date" name="field_name[]" value=""/> <input placeholder="Şəxsiyyət Vəsiqəsinin nömrəsi" type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="add_button" title="Add field"><img src="/dizayn/img/add-icon.png"/></a>
+    	<input id="inp" placeholder="Ad" type="text" name="field_name[]" value=""/><input id="inp" placeholder="Soyad" type="text" name="field_name[]" value=""/><input id="inp" placeholder="Ata adı	" type="text" name="field_name[]" value=""/><input id="inp" type="date" name="field_name[]" value=""/> <input id="inp" placeholder="Şəxsiyyət Vəsiqəsinin nömrəsi" type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="add_button" title="Add field"><img src="/dizayn/img/add-icon.png"/></a>
 
     </div>
 
 </div>
 <input type="submit" name="submit" value="Ödəmə"/>
 </form>
-</body>
+
 
 @stop
